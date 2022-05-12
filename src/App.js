67209a0664ff96c6427aae5825ccd9316518a23c
lucascorreaog <lucascorreaog@gmail.com>
2022-05-12 14:48:26 -0300

    feat: components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import GlobalStyle from "./global/global";
+import Input from "./components/Input";
+import Button from "./components/Button";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Input placeholder="CNPJ" />
+      <Input placeholder="EMPRESA" />
+      <Button>CADASTRAR</Button>
     </>
   );
 }
